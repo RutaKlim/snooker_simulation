85d368e455622b30b458bf843e649b6087ceb5fd
Ruta <rutaklim7@gmail.com>
2026-08-02 14:12:04 +0100

    project works normally

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import "./style.css";
+import javascriptLogo from "./assets/javascript.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import { setupCounter } from "./counter.js";
+
+// document.querySelector("#app").innerHTML = `
+//   <h1 class="text-5xl font-bold text-orange-600">
+//       I hope this works
+//   </h1>
+//   `;
